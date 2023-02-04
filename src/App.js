0372ad4951e3354ddbe88f8811a6b5a951461b38
igorp <igorprohorchenko@gmail.com>
2023-02-04 22:01:49 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.scss';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import { Home, MealDetails, Error, Category } from "./pages/index";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 export function App() {
   return (
-    <div className="App">
-
-    </div>
+      <BrowserRouter>
+        <Header />
+        <Sidebar />
+        <Routes>
+          <Route path = "/" element = {<Home />} />
+          <Route path = "/meal/:id" element = {<MealDetails />} />
+          <Route path = "/meal/category/:name" element = {<Category />} />
+          <Route path  = "*" element = {<Error />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
